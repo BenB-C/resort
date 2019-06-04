bc329b856020946060b3dd899832a2fbbfd6b402
Ben Bennett-Cauchon and Micah Zaayer <ben@>
2019-06-04 15:05:17 -0700

    add more $ functions and mildly infuriating buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,56 @@
 $(document).ready(function() {
+  var btn1Shown;
+  var btn2Shown;
+
   $("h1").click(function()  {
     $("img").slideToggle();
-  })
-  $("p").hover(function() {
-    
-  })
+  });
+  // $("p").hover(function() {
+  //   $("p").css("color", "green")
+  // })
 
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("greenText");
+    $("#btn1").show();
+    $("#btn2").hide();
+    btn1Shown = true;
+    btn2Shown = false;
+    $("button#yellow").hide();
+    $("button#red").hide();
+  });
 
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellowText");
+  });
 
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("redText");
+  });
 
+  $("#btn1").hover(function() {
+    if (btn1Shown) {
+      $("#btn1").hide();
+      $("#btn2").show();
+      btn1Shown = false;
+      btn2Shown = true;
+    }
+  });
 
+  $("#btn2").hover(function() {
+    if (btn2Shown) {
+      $("#btn2").hide();
+      $("#btn1").show();
+      btn2Shown = false;
+      btn1Shown = true;
+    }
+  });
 
-
-
-
+function f() {
+  alert("f called");
+}
 
 
 
